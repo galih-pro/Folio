dc0a895484a0b9c8bb05c08fb526af99df238815
galih <galihmuhammad.pro@gmail.com>
2022-07-20 19:12:44 +0700

    fixing code and adding assets

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const btn = document.getElementById("navBtn");
 const a = document.getElementById("imgLight");
 const b = document.getElementById("imgDark");
 btn.addEventListener('click', function () {
+    this.classList.toggle('notShow');
     document.getElementById("navList").classList.toggle('off');
     button()
 });
@@ -9,7 +10,6 @@ btn.addEventListener('click', function () {
 function button(){
     const a = document.getElementById("imgLight");
     const b = document.getElementById("imgDark");
-    console.log(a.classList)
     if (a.classList.value === "off active") {
         a.classList.remove("active");
         b.classList.add("active")
